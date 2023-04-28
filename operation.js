448e61ec278d729b6b4514cb84805ded3ccd8593
raj <rpande400@gmail.com>
2023-04-28 13:26:42 +0530

    first commit

---
+++ operation.js
@@ -0,0 +1,21 @@
+
+// Modules
+
+// module.exports.add = function (a, b) {
+//     return a + b;
+// }
+// module.exports.multiply=function(a,b){
+//     return a*b;
+// }
+
+// exports.div=function(a,b){
+//     return a/b;
+// }
+
+// exports.modules=function(a,b){
+//     return a%b;
+// }
+
+// var sub=function(a, b){
+//     return a-b;
+// }
